c8093ad020fe04ce552e3eeed7b84cb11a1041a3
Kacper Stroisz <kacperstroisz@wp.pl>
2018-04-16 12:08:06 +0200

    Add files

---
+++ config.js
@@ -0,0 +1,5 @@
+module.exports = {
+    'GOOGLE_CLIENT_ID': '401628433622-v3jbrktjjk14l0ab6r6nsd92k1h9q9as.apps.googleusercontent.com',
+    'GOOGLE_CLIENT_SECRET': 'AfgRp-ZgUbG0fSiSbEgJdYDp',
+    'CALLBACK_URL': 'http://localhost:3000/auth/google/callback'
+};
